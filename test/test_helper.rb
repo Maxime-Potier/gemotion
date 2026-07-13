1ba491934ef9bcf86bf09cf5bc3e4e35ea107472
Vadim Kassin <vgzulus@gmail.com>
2026-07-14 00:04:55 +0300

    Isolate test database configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,14 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+test_database = ActiveRecord::Base.connection_db_config.database.to_s
+unless test_database.end_with?("_test")
+  raise <<~MESSAGE
+    Refusing to run tests against #{test_database.inspect}.
+    Configure TEST_DATABASE_URL to point to a dedicated database ending in _test.
+  MESSAGE
+end
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
